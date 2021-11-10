95067d80f2ea053d195956c6b07619612f850a2d
imjord <dean.jay.goat@gmail.com>
2021-11-09 18:26:45 -0700

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import logo from './assets/logo.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} style={styles.logo} />
+      <Text style={styles.instructions}>To share a photo from your phone wqith a friend press the button beloew!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +20,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    width: 350,
+    height: 150,
+    marginBottom: 10,
+    borderRadius: 50
+  },
+  instructions: {
+    color: "blue",
+    fontSize: 18,
+    marginHorizontal: 15,
+  }
 });
